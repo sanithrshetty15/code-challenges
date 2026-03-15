f9b926c96c5681a94154fcfd30be42a3cd79d9cc
Sanith R Shetty <sanithrshetty15@gmail.com>
2026-03-15 21:12:30 +0530

    slight changes in data base

--- db/migrate/20260315135953_create_challenges.rb
+++ db/migrate/20260315135953_create_challenges.rb
@@ -1,10 +1,10 @@
 class CreateChallenges < ActiveRecord::Migration[8.1]
   def change
     create_table :challenges do |t|
-      t.string :title, null:false
-      t.text :description, null:false
-      t.date :start_date, null:false
-      t.date :end_date, null:false
+      t.string :title, null: false
+      t.text :description, null: false
+      t.date :start_date, null: false
+      t.date :end_date, null: false
 
       t.timestamps
     end
