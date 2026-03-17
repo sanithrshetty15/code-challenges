0a67e294b1d7bfb9b93e864f3e011a75c4033fdf
Sanith R Shetty <sanithrshetty15@gmail.com>
2026-03-17 21:52:04 +0530

    small changes

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,9 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 end
-class ApplicationRecord < ActiveRecord::Base
-  primary_abstract_class
-end
-class ApplicationRecord < ActiveRecord::Base
-  primary_abstract_class
-end
