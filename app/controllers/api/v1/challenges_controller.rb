f9bfcffa38264490da720e110e669765daa8abd3
sanithrshetty15 <sanithrshetty15@gmail.com>
2026-03-18 23:33:08 +0530

    small changes

---
+++ app/controllers/api/v1/challenges_controller.rb
@@ -0,0 +1,21 @@
+module Api
+    module V1
+        class ChallengesController < ApplicationController
+            def index
+            end
+
+
+            def create
+            end
+
+            def show
+            end
+
+            def update
+            end
+
+            def destroy
+            end
+        end
+    end
+end
