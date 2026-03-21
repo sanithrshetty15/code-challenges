462ae737c076806bdc8c37377fee948e000d82d2
sanithrshetty15 <sanithrshetty15@gmail.com>
2026-03-21 15:18:42 +0530

    added api routes for all funtions

--- app/controllers/api/v1/challenges_controller.rb
+++ app/controllers/api/v1/challenges_controller.rb
@@ -1,20 +1,36 @@
 module Api
     module V1
         class ChallengesController < ApplicationController
+           # GET    /api/v1/challenges
             def index
+                # show all challenge
+                @Challenges = Challenge.all
+                render json: @Challenges
             end
 
-
+            #POST   /api/v1/challenges   
             def create
+                #create single challenge
+
             end
 
+
+             #GET    /api/v1/challenges/:id
             def show
+                #show single challenge
             end
 
+
+            #PATCH  /api/v1/challenges/:id
+            #PUT    /api/v1/challenges/:id
             def update
+                 #update single challenge
             end
 
+
+            # DELETE /api/v1/challenges/:id
             def destroy
+                 #delete single challenge
             end
         end
     end
